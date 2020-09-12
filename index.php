eadb524eec3e5784630334137facec64e2a40a95
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-09-12 22:24:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ if ($allow == false) {
   <script src="assets/js/pnotify.js"></script>
   <script>
     urlRoute
-      .folderUrl('/Key-PreV1')
+      .folderUrl('/')
       .setPreviousCode('Key.Home')
       .setBaseUrl('https://didyouknow.ivanisafuckingnonce.gq/')
       .checkCurrent('https://didyouknow.ivanisafuckingnonce.gq/');
