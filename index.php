9f2e71aee1084b52d4083d57c41ec98da0dce5a2
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-09-12 22:20:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ if ($allow == false) {
   <script src="assets/js/pnotify.js"></script>
   <script>
     urlRoute
-      .folderUrl('https://didyouknow.ivanisafuckingnonce.gq')
+      .folderUrl('/')
       .setPreviousCode('Key.Home')
       .setBaseUrl('https://didyouknow.ivanisafuckingnonce.gq/')
       .checkCurrent('https://didyouknow.ivanisafuckingnonce.gq/');
